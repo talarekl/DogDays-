bb44873dac443589a5d6c057fe28f673c9c57e20
talarekl <83030712+talarekl@users.noreply.github.com>
2021-05-11 15:00:55 -0400

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,24 +1,38 @@
-// Your web app's Firebase configuration
-       // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-       var firebaseConfig = {
-        apiKey: "AIzaSyAT1a_otRiLhU06LV43GM4IzqVrHm_eREY",
-        authDomain: "cs-312-final-dog-days.firebaseapp.com",
-        projectId: "cs-312-final-dog-days",
-        storageBucket: "cs-312-final-dog-days.appspot.com",
-        messagingSenderId: "253993037694",
-        appId: "1:253993037694:web:98b007df2f922acd598d2f",
-        measurementId: "G-FQW3CBLDEG"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-      firebase.analytics();
+var firebaseConfig = {
+  apiKey: "AIzaSyAzjL9l25mAmgOqy1PuNhNkQAhFGEGkUEA",
+  authDomain: "dog-days-51554.firebaseapp.com",
+  databaseURL: "https://dog-days-51554-default-rtdb.firebaseio.com",
+  projectId: "dog-days-51554",
+  storageBucket: "dog-days-51554.appspot.com",
+  messagingSenderId: "223722049182",
+  appId: "1:223722049182:web:3b421b741498663a88758f",
+  measurementId: "G-QYD9PCSK45"
+};
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
 
-      function signUp(){
-        var email = document.getElementById("email");
-        var psw = document.getElementsById("password");
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
 
-        const create = auth.createUserWithEmailAndPassword(email.value, psw.value);
-        create.catch(e => alert(e.message));
-         alert("Signed In");
 
-      };
+firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth;
+
+  firebase.initializeApp(firebaseConfig);
+  
+  const auth = firebase.auth();
+  
+  
+  function signUp(){
+      
+      var email = document.getElementById('email');
+      var password = document.getElementById('password');
+      
+      const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+      promise.catch(e => alert(e.message));
+      
+      alert("Signed Up");
+  }
+  
